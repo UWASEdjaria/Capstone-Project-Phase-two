f868a7ef28c97414c399167f98c792317824baa6
Uwase Djaria <uwasedjaria41@gmail.com>
2025-11-14 20:48:23 +0200

    add header and update home page with styles

--- src/app/lab1/app/page.tsx
+++ src/app/lab1/app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Header from '../components/layout/Header'
 
 function page() {
   return (
     <main className="min-h-screen bg-white/87 flex flex-col justify-center p-4 md:p-8 lg:p-16">
-
+       {/*navbar*/}
+       <Header/>
        {/* Heading */}
       <h1 className="text-7xl text-left font-serif sm:text-2xl md:text-8xl lg:text-9xl px-4 mb-2 p-1 md:px-15 lg:px-15 text-black/90 font-light">
         Human   <br/> stories & ideas
@@ -13,7 +15,7 @@ function page() {
       </p>
       
       {/* Button */}
-      <button className="bg-green-600 md:bg-black  text-white px-4 md:px-15 py-1 rounded-full font-medium text-xl hover:bg-green-800 transition-colors w-60">
+      <button className="bg-green-600 md:bg-black  text-white px-4 py-4 md:px-15  rounded-full font-medium text-xl hover:bg-green-800 transition-colors w-60">
        Start reading
       </button>
     </main>
---
+++ src/app/lab1/components/layout/Header.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import React from 'react'
+
+function Header() {
+  return (
+    <header className="w-full border-b  border-black">
+        <div className="max-w-6xl mx-auto flex items-center justify-between py-4 px-4">
+            {/* Logo */}
+          <Link href='/' >
+          <h1 className='text-2xl text-black font-semibold cursor-pointer'>Medium</h1> 
+          </Link>
+            {/* Navigation Links */}
+             <nav className="flex  flex-col md:flex-row   justify-between py-4 gap-3  text-black">
+          <Link href="/about" className="hover:text-black">Our story</Link>
+          <Link href="/contact" className="hover:text-black">Membership</Link>
+          <Link href="/login" className="hover:text-black">Write</Link>
+           <Link href="/contact" className="hover:text-black">sign in</Link>
+
+          <Link
+            href="/register"
+            className="bg-black text-white px-2 py-2 rounded-full hover:bg-gray-900">
+            Get Started
+            </Link>
+            
+          </nav>
+        </div>
+    </header>
+  )
+}
+
+export default Header
