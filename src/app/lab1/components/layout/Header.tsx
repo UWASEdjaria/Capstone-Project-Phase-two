202bd6efbb2c22838ff3cdde5c0167dda9950e11
Uwase Djaria <uwasedjaria41@gmail.com>
2025-11-16 14:20:22 +0200

    Save Header changes before switching branch

--- src/app/lab1/components/layout/Header.tsx
+++ src/app/lab1/components/layout/Header.tsx
@@ -13,23 +13,15 @@ function Header() {
              <nav className="flex  flex-col md:flex-row   justify-between py-4 gap-3  text-black">
           <Link href="/" className="hover:text-black">Home</Link>
           <Link href="/about" className="hover:text-black">Our story</Link>
-          <Link href="/membership" className="hover:text-black">Membership</Link>
-<<<<<<< HEAD
-          <Link href="/login" className="hover:text-black">Write</Link>
-           <Link href="/contact" className="hover:text-black">sign in</Link>
+<Link href="/membership" className="hover:text-black">Membership</Link>
+<Link href="/write" className="hover:text-black">Write</Link>
+ <Link href="/sign-in" className="hover:text-black">sign in</Link>
 
-          <Link
-            href="/register"
-=======
-          <Link href="/write" className="hover:text-black">Write</Link>
-           <Link href="/sign-in" className="hover:text-black">sign in</Link>
-
-          <Link
-            href="/signUp"
->>>>>>> 4018e87 (Add src/app pages to main)
-            className="bg-black text-white px-2 py-2 rounded-full hover:bg-gray-900">
-            Get Started
-            </Link>
+<Link
+  href="/signUp"
+  className="bg-black text-white px-2 py-2 rounded-full hover:bg-gray-900">
+  Get Started
+  </Link>
             
           </nav>
         </div>
